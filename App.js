82af9ee4eecb2a7c7bd7775331b4642968eaa9ac
BhaktiKshirsagar <bhaktiksh10@gmail.com>
2024-07-12 12:13:52 +0530

    Day-1

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am H1 Tag"),
+      React.createElement("h2", {}, "I am H2 Tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am H1 Tag"),
+      React.createElement("h2", {}, "I am H2 Tag"),
+    ]),
+  ]);
+  
+  console.log(parent);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(parent);
